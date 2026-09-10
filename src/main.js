d9773d2f93940fa85a3e4acde1bed2c4d8da14c5
Lokesh Rao <lokeshh.rao1@gmail.com>
2026-09-10 12:36:51 +0530

    finish NASA APOD viewer

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
-console.log("NASA project started!");
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.querySelector("#app").innerHTML = "<p>Loading...</p>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+
+    let media;
+
+    if (data.media_type === "image") {
+      media = `<img src="${data.url}" />`;
+    } else if (data.url.includes("youtube")) {
+      media = `<iframe src="${data.url}" allowfullscreen></iframe>`;
+    } else {
+      media = `<video src="${data.url}" controls></video>`;
+    }
+
+    document.querySelector("#app").innerHTML = `
+  <h1>${data.title}</h1>
+  <p class="date">${data.date}</p>
+  ${media}
+  <p>${data.explanation}</p>
+`;
+  })
+  .catch(err => {
+    document.querySelector("#app").innerHTML = `
+      <p>Error: ${err.message}</p>
+    `;
+  });
